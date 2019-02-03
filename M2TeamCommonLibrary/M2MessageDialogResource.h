981e5fc14a7006ce4da37d55ca26dffc5db5e737
Naruto Mouri <Mouri_Naruto@outlook.com>
2019-02-04 03:45:21 +0800

    Add M2Win32GUIHelpers.

---
+++ M2TeamCommonLibrary/M2MessageDialogResource.h
@@ -0,0 +1,12 @@
+﻿/*
+ * PROJECT:   M2-Team Common Library
+ * FILE:      M2MessageDialogResource.h
+ * PURPOSE:   Definition for the message dialog resource
+ *
+ * LICENSE:   The MIT License
+ *
+ * DEVELOPER: Mouri_Naruto (Mouri_Naruto AT Outlook.com)
+ */
+
+#define IDD_MESSAGE_DIALOG              105
+#define IDC_MESSAGE_DIALOG_EDIT         1003
